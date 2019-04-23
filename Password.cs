97e430a050861df621f8ea77be5a80dffbc7e01c
Ian Norris <icstatic@gmail.com>
2019-04-23 19:38:21 +0100

    Working version

---
+++ Password.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashlaneTo1Password
+{
+	public class Password
+	{
+		public string domain { get; set; }
+		public string email { get; set; }
+		public string login { get; set; }
+		public string note { get; set; }
+		public string password { get; set; }
+		public string secondaryLogin { get; set; }
+		public string title { get; set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace DashlaneTo1Password
+{
+	class Program
+	{
+		static string ToCsv( string Input )
+		{
+			return $"\"{Input.Replace("\"", "\"\"")}\"";
+		}
+
+		static void Main(string[] args)
+		{
+			string JsonData = File.ReadAllText(args[0]);
+			string OutputDir = Path.GetDirectoryName(args[0]);
+
+			string OutputPasswords = Path.Combine(OutputDir, "1PasswordImport_Passwords.csv");
+			string PasswordsFile = "";
+
+			//Passwords format:
+			//title,website,username,password,notes,Associated Email,Secondary Login, custom field 3, custom field 4, etc
+
+			JObject JsonObject = JObject.Parse(JsonData);
+			JArray Passwords = (JArray)JsonObject["AUTHENTIFIANT"];
+
+			foreach( var Password in Passwords )
+			{
+				var NewPassword = Password.ToObject<Password>();
+
+				PasswordsFile += $"{ToCsv(NewPassword.title)},{ToCsv(NewPassword.domain)},{ToCsv(NewPassword.login)},{ToCsv(NewPassword.password)},{ToCsv(NewPassword.note)},{ToCsv(NewPassword.email)},{ToCsv(NewPassword.secondaryLogin)},\r\n";
+			}
+
+			File.WriteAllText(OutputPasswords, PasswordsFile);
+		}
+	}
+}
